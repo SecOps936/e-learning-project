55921e6c22ab0d89bb043afb6afe4e5b20ec2893
DevOps <heriwambo27@gmail.com>
2025-10-01 14:32:09 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,16 +1,1 @@
-<?php
-// db.php - Database connection
 
-$servername = "localhost"; // or your server
-$username = "heri";        // default username for MySQL
-$password = "1234";            // default password for MySQL (empty for XAMPP)
-$dbname = "StudyPortal";   // updated database name
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-?>
